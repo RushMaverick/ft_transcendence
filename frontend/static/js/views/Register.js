faca86015341a5598d8cdfe812f47b288562e270
ynslee <112563002+ynslee@users.noreply.github.com>
2024-06-06 06:02:41 +0300

    <feat> login&register with all the links ready

---
+++ frontend/static/js/views/Register.js
@@ -0,0 +1,24 @@
+import AView from "./AView.js";
+
+export default class extends AView {
+	constructor(params){
+		super(params);//call the constructor of the parent class
+		this.setTitle("Register");
+	}
+
+	async getHtml(){
+		return `
+		<h1>Register</h1>
+			<p>Already have an account? <a href="/login" data-link>Login here</a></p>
+			<br>
+			<form action="/register.php" class="registerForm">
+				<div class="input-group">
+					<p>Please type your username and password</p>
+					<label for="username" class="label">Username</label>
+					<input type="text" id="username" class="input">
+					<label for="password" class="label">Password</label>
+					<input type="text" id="password" class="input">
+				</div>
+		`;
+	}
+}
