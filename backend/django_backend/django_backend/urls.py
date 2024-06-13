b752537c4491f541b12010388ae48c9a1976d067
Juan Esteban Pelaez Hoyos <juanes.pelaez18@gmail.com>
2024-06-13 14:39:01 +0300

    chore(avatar): Removed the debug flag from urls.py. It was tested it and we can still upload images.

--- backend/django_backend/django_backend/urls.py
+++ backend/django_backend/django_backend/urls.py
@@ -41,5 +41,4 @@ urlpatterns = [
     path('profile/', include('user.urls'))
 ] 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
