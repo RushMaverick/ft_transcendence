0d95356e0e204db1998339cc155ad657be6e1351
rosa m <makinen.rosa@gmail.com>
2024-06-13 10:48:37 +0300

    refactor: added universal dom funcs to AView, implemented to login & register

--- frontend/static/js/views/AView.js
+++ frontend/static/js/views/AView.js
@@ -1,5 +1,3 @@
-import AView from "./AView.js";
-
 export default class {
 	constructor(params){
 		this.params = params;
@@ -14,11 +12,50 @@ export default class {
 	}
 
 	updateView(...elements){
-		const app = document.querySelector('main');
-		app.innerHTML = '';
+		const container = document.querySelector('main');
+		container.innerHTML = '';
 		elements.forEach((element) => {
 			if (element)
-				app.appendChild(element);
+				container.appendChild(element);
 		});
 	}
+
+	createHeader1(text) {
+        const header = document.createElement('h1');
+        header.textContent = text;
+        return header;
+    }
+
+	createHeader2(text) {
+        const header = document.createElement('h2');
+        header.textContent = text;
+        return header;
+    }
+
+    createParagraph(text) {
+        const p = document.createElement('p');
+        p.textContent = text;
+        return p;
+    }
+
+	createAnchor(text){
+		const a = document.createElement('a');
+        a.textContent = text;
+        return a;
+	}
+
+	createForm(name){
+		const form = document.createElement('form');
+		form.classList.add(name);
+		return form
+	}
+
+	createButton(functionality, text){
+		const button = document.createElement('button');
+		button.classList.add(functionality, 'button');
+		button.textContent = text;
+		return button;
+	}
+
+
 }
