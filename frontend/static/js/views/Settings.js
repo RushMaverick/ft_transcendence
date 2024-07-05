a151495464e11b6d9cdcb1c5b1742e3eae46c77f
rosa m <makinen.rosa@gmail.com>
2024-07-05 10:21:12 +0300

    feat: added function to handle form submit

--- frontend/static/js/views/Settings.js
+++ frontend/static/js/views/Settings.js
@@ -7,17 +7,6 @@ export default class extends AView {
 		this.setTitle('Settings');
 	}
 
-	setLanguage(button){
-		console.log(button.getAttribute('language'));
-		console.log('languages event listener ok');
-		if (button.matches('lang-toggle')) {
-			console.log('we should change the lang');
-			const language = button.getAttribute('language');
-			window.localStorage.setItem('language', language);
-			document.dispatchEvent(new CustomEvent('viewUpdated'));
-		}
-	}
-
 	async getHtml(){
 
 		const title = this.createHeader('header', 'tervetuloo', 'h2');
@@ -29,24 +18,66 @@ export default class extends AView {
 		const usernameInput = textInputField('username', 'Username', 'username', 'text');
 		const passwordInput = textInputField('password', 'Password', 'password', 'password');
 		const confirmPasswordInput = textInputField('password-again', 'Confirm password', 'confirm-password', 'password');
-		const signupButton = this.createButton('save', 'save');
+		const signupButton = this.createButton('submit', 'save');
+		
 		form.appendChild(firstnameInput);
 		form.appendChild(lastnameInput);
 		form.appendChild(usernameInput);
 		form.appendChild(passwordInput);
 		form.appendChild(confirmPasswordInput);
 		form.appendChild(signupButton);
+
+		form.addEventListener('submit', this.handleFormSubmit.bind(this));
 		
 		const buttonEn = this.createLanguageButton('english', 'lang-toggle', 'English');
 		const buttonFi = this.createLanguageButton('finnish', 'lang-toggle', 'Finnish');
 		const buttonSp = this.createLanguageButton('spanish', 'lang-toggle', 'Spanish');
 		
-		buttonEn.addEventListener("click", this.setLanguage(buttonEn));
-		buttonFi.addEventListener("click", this.setLanguage(buttonFi));
-		buttonSp.addEventListener("click", this.setLanguage(buttonSp));
-		
 		window.localStorage.setItem('page', 'Settings');
 		this.updateView(title, form, buttonEn, buttonFi, buttonSp);
 		return ;
 	}
+
+	async handleSettingsFormSubmit(event) {
+        event.preventDefault(); // Prevent the default form submission behavior
+
+        const username = event.target.username.value;
+        const password = event.target.password.value;
+        const confirmPassword = event.target['confirm-password'].value;
+
+        if (password !== confirmPassword) {
+            alert("Passwords do not match!");
+            return;
+        }
+
+		const newID = uuidv4();
+        // Create the JSON object to be sent
+        const data = {
+            id: newID,
+            username: username,
+            password: password,
+            // profile: {
+            //     // avatar: ,
+            // }
+        };
+
+        try {
+            const response = await fetch(`${import.meta.env.VITE_API_ENDPOINT}/register`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            const responseData = await response.json();
+            console.log(responseData);
+        } catch (error) {
+            console.error('There was a problem with the fetch operation:', error);
+        }
+    }
 }
