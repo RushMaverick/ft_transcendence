2fd5655537ba0eb20598789687a07e2e536d8999
Teemu Spoof <steemum@hotmail.com>
2024-07-12 15:46:41 +0300

    docs (api documentation): add swagger and redoc api docs with drf_spectacular
    Once you run make the docs are available at `http://localhost:8000/api/schema/swagger-ui/` and `http://localhost:8000/api/schema/redoc/`

--- backend/django_backend/django_backend/settings.py
+++ backend/django_backend/django_backend/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_spectacular'
 ]
 
 MIDDLEWARE = [
@@ -160,6 +161,7 @@ REST_FRAMEWORK = {
 	'DEFAULT_AUTHENTICATION_CLASSES': [
 		'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+	'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
 SIMPLE_JWT = {
--- backend/django_backend/django_backend/urls.py
+++ backend/django_backend/django_backend/urls.py
@@ -25,7 +25,7 @@ from rest_framework_simplejwt.views import (
 
 from rest_framework import routers
 from user.views import UserViewSet
-# from rest_demo import views as rest_views
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 router = routers.DefaultRouter()
 # router.register(r'users', rest_views.UserViewSet, basename='users')
@@ -40,7 +40,10 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('profile/', include('user.urls')),
     path('friends/', include('friends.urls')),
-    path('matches/', include('match.urls'))
+    path('matches/', include('match.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
