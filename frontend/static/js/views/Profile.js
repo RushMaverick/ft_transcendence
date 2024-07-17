6f91ac64635de3232abf757d9f93c46ed7530875
ynslee <112563002+ynslee@users.noreply.github.com>
2024-07-17 15:46:02 +0300

    feat: profile page changing

--- frontend/static/js/views/Profile.js
+++ frontend/static/js/views/Profile.js
@@ -4,12 +4,6 @@ export default class extends AView {
 	constructor(params){
 		super(params);//call the constructor of the parent class
 		this.setTitle("Profile");
-		
-		this.userData = {
-			name: 'Test Name',
-			email: 'testmail@test.com',
-			pictureUrl: 'https://snworksceo.imgix.net/drt/7a10426e-6523-4304-8532-2d230f9bab9b.sized-1000x1000.jpg?w=1000&dpr=2'
-		};
 	}
 
 	fetchName(userData){
@@ -19,15 +13,7 @@ export default class extends AView {
 		
 		return base;
 	}
-	
-	fetchEmail(userData){
-		const base = document.createElement('section');
-		const email = this.userData.email;
-		base.textContent = email;
-		
-		return base;
-	}
-	
+
 	fetchPicture(userData){
 		const img = document.createElement('img');
 		img.src = this.userData.pictureUrl;
@@ -40,15 +26,13 @@ export default class extends AView {
 		const header = this.createHeader('header', 'testheader', 'h2');
 		const userNameBase = this.createParagraph('usernamebase');
 		const userName= this.fetchName(userData);
-		const userEmailBase = this.createParagraph('emailbase');
-		const userEmail = this.fetchEmail(userData);
 		const profilePicutre = this.fetchPicture(userData);
         const message = this.createParagraph('message');
         const settings = this.createLink('link2', 'Change settings from here', '/settings');
         const stats = this.createLink('link1', 'Change settings from here', '/stats');
 
 		window.localStorage.setItem('page', 'Profile');
-		this.updateView(header, userNameBase, userName, userEmailBase, userEmail, profilePicutre, message, settings, stats);
+		this.updateView(header, userNameBase, userName, profilePicutre, message, settings, stats);
 		return ;
 	}
 }
