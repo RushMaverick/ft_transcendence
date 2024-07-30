06430c5df88a2d3ffd82d16d3ed06f6f2bd75fa6
Juan Esteban Pelaez Hoyos <juanes.pelaez18@gmail.com>
2024-07-30 16:14:10 +0300

    chore: added users in the pong consumer, in that way players can play game

--- backend/django_backend/django_backend/routing.py
+++ backend/django_backend/django_backend/routing.py
@@ -4,6 +4,6 @@ from pong import consumers as game_consumers
 from user import consumers as online_consumer
 
 websocket_urlpatterns = [
-    re_path(r"ws/game/(?P<room_name>\w+)/$", game_consumers.GameConsumer.as_asgi()),
+    re_path(r"ws/game/(?P<room_id>\d+)/$", game_consumers.GameConsumer.as_asgi()),
     re_path(r"ws/online_status/$", online_consumer.OnlineConsumer.as_asgi()),
 ]
--- backend/django_backend/pong/consumers.py
+++ backend/django_backend/pong/consumers.py
@@ -1,29 +1,42 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from pong.pong_game import Pong, Player, Games
+from rooms.models import Room
+from user.models import User
+from channels.db import database_sync_to_async
 
 class GameConsumer(AsyncWebsocketConsumer):
     def __init__(self, *args, **kwargs):
         self.player = None
         self.pong_game: Pong = None
+        self.user = None
+        self.room = None
         super().__init__(*args, **kwargs)
 
 
     async def connect(self):
-        # user = self.scope['user']
-        # print(f"User: {user}")
-        # if not user.is_authenticated:
-        #     print("User not authenticated")
-        #     await self.close()
+        self.game_room_id = self.scope["url_route"]["kwargs"]["room_id"]
+        self.user = self.scope["user"]
 
-        self.game_room = self.scope["url_route"]["kwargs"]["room_name"]
-        self.room_group_name = f"game_{self.game_room}"
+        # Retrieve the Room instance
+        try:
+            self.room = await self.get_room(self.game_room_id)
+        except Room.DoesNotExist:
+            await self.close()
+            return
+
+        # Check if the user is part of the room
+        if not await self.is_user_in_room(self.user, self.room):
+            await self.close()
+            return
+
+        self.room_group_name = f"game_{self.game_room_id}"
 
         # Add game to games
-        if self.game_room not in Games.games:
-            Games.create_game(self.game_room)
+        if self.game_room_id not in Games.games:
+            Games.create_game(self.game_room_id)
 
-        self.pong_game = Games.games[self.game_room]
+        self.pong_game = Games.games[self.game_room_id]
 
         # Socket stuff
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
@@ -31,20 +44,20 @@ class GameConsumer(AsyncWebsocketConsumer):
             self.pong_game.add_channel_layer(self.channel_layer)
         if not self.pong_game.room_group_name:
             self.pong_game.add_room_group_name(self.room_group_name)
+
         # Add player to game
         if not self.pong_game.player1:
-            self.player = Player(1, "Player 1")
+            self.player = Player(1, self.user.username)
             self.pong_game.add_player(self.player)
         elif not self.pong_game.player2:
-            self.player = Player(2, "Player 2")
+            self.player = Player(2, self.user.username)
             self.pong_game.add_player(self.player)
 
         if self.pong_game.player1 and self.pong_game.player2:
-                Games.start_game(self.game_room)
+            Games.start_game(self.game_room_id)
 
-        print (f"Connected to {self.room_group_name}", flush=True)
+        print(f"Connected to {self.room_group_name} as {self.user.username}", flush=True)
         await self.accept()
-
 
     async def disconnect(self, close_code):
         print(f"{self.player.name} disconnected from room {self.game_room}", flush=True)
@@ -73,4 +86,12 @@ class GameConsumer(AsyncWebsocketConsumer):
         state = event["state"]
         # Send message to WebSocket
         await self.send(text_data=json.dumps(state))
+
+    @database_sync_to_async
+    def get_room(self, room_id):
+        return Room.objects.get(id=room_id)
+   
+    @database_sync_to_async
+    def is_user_in_room(self, user, room):
+        return room.users.filter(id=user.id).exists()
 
